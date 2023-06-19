adc5a1a82c907758601c8ae62604d41391a72efe
amy null <null@amy.gg>
2023-06-19 16:22:33 -0400

    🐛 fix: Permissions and symlinks!

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use std::path::{Path, PathBuf};
 use eyre::Result;
 use floppy_disk::prelude::*;
 use tokio::io::AsyncWriteExt;
-use tracing::{debug, error, warn};
+use tracing::{error, trace, warn};
 
 pub struct DiskDrive<
     'a,
@@ -83,23 +83,37 @@ where
         let src_path = src_path.unwrap_or_else(|| PathBuf::from("/"));
         let dest_path = dest_path.unwrap_or_else(|| PathBuf::from("/"));
         let paths = if src.metadata(&src_path).await?.is_file() {
-            debug!("copying src file {}", src_path.display());
+            trace!("copying src file {}", src_path.display());
             let mut out = BTreeSet::new();
             out.insert(src_path);
             out
         } else {
-            debug!("copying src dir {}", src_path.display());
+            trace!("copying src dir {}", src_path.display());
             nyoom::walk_ordered(src, src_path).await?
         };
         for src_path in paths {
-            debug!("copy {} -> {}", src_path.display(), dest_path.display());
-            let metadata = <F1 as FloppyDisk<'a>>::metadata(src, &src_path).await?;
+            trace!("processing src_path: {}", src_path.display());
+            let metadata = match <F1 as FloppyDisk<'a>>::metadata(src, &src_path).await {
+                Ok(m) => m,
+                Err(_) => <F1 as FloppyDisk<'a>>::symlink_metadata(src, &src_path).await?,
+            };
             let file_type = metadata.file_type();
             if file_type.is_dir() {
+                trace!("copy dir {} -> {}", src_path.display(), dest_path.display());
                 Self::copy_dir_to_memfs(src, dest, &src_path, &dest_path).await?;
             } else if file_type.is_file() {
+                trace!(
+                    "copy file {} -> {}",
+                    src_path.display(),
+                    dest_path.display()
+                );
                 Self::copy_file_to_memfs(src, dest, &src_path, &dest_path).await?;
             } else if file_type.is_symlink() {
+                trace!(
+                    "copy symlink {} -> {}",
+                    src_path.display(),
+                    dest_path.display()
+                );
                 Self::add_symlink_to_memfs(src, dest, &src_path, &dest_path).await?;
             } else {
                 error!("unknown file type for source path {src_path:?}");
@@ -115,7 +129,9 @@ where
         src_path: &Path,
         dest_path: &Path,
     ) -> Result<()> {
-        debug!("creating file {dest_path:?}");
+        let dest_path = dest_path.join(src_path);
+        let dest_path = dest_path.as_path();
+        trace!("creating file {dest_path:?}");
         if let Some(memfs_parent) = dest_path.parent() {
             dest.create_dir_all(memfs_parent).await?;
         }
@@ -125,31 +141,55 @@ where
             .open(src, src_path)
             .await?;
         {
+            if let Some(parent) = dest_path.parent() {
+                trace!("creating dest file parents");
+                dest.create_dir_all(parent).await?;
+            }
+
             let dest_metadata = <F2 as FloppyDisk>::metadata(dest, dest_path).await;
-            let dest_handle = <F2::OpenOptions>::new();
-            let dest_handle = dest_handle.create(true).write(true);
-
-            // if dest metadata doesn't exist, just copy directly
+            // if dest doesn't exist, just copy directly
             if dest_metadata.is_err() {
-                debug!("dest {dest_path:?} doesn't exist, copying directly!");
-                let mut dest_handle: <F2 as FloppyDisk>::File =
-                    dest_handle.open(dest, dest_path).await?;
-
+                trace!("dest file {dest_path:?} doesn't exist, copying directly!");
+                let mut dest_handle: <F2 as FloppyDisk>::File = <F2::OpenOptions>::new()
+                    .create(true)
+                    .read(true)
+                    .write(true)
+                    .create_new(true)
+                    .open(dest, dest_path)
+                    .await?;
                 tokio::io::copy(&mut src_handle, &mut dest_handle).await?;
+
+                // copy permissions
+                let src_metadata = src_handle.metadata().await?;
+                let src_permissions = src_metadata.permissions();
+                let mode = <<F1 as FloppyDisk<'_>>::Permissions as FloppyUnixPermissions>::mode(
+                    &src_permissions,
+                );
+
+                let permissions = <<F2 as FloppyDisk>::Permissions>::from_mode(mode);
+                let uid = src_metadata.uid()?;
+                let gid = src_metadata.gid()?;
+
+                <F2 as FloppyDiskUnixExt>::chown(dest, dest_path, uid, gid).await?;
+                <F2 as FloppyDisk>::set_permissions(dest, dest_path, permissions).await?;
+
                 return Ok(());
             }
+
+            let mut dest_handle: <F2 as FloppyDisk>::File = <F2::OpenOptions>::new()
+                .read(true)
+                .write(true)
+                .open(dest, dest_path)
+                .await?;
 
             // if dest exists and is a dir, copy into it
             let dest_metadata = dest_metadata?;
             if dest_metadata.is_dir() {
-                debug!("copying into dir {dest_path:?}");
+                trace!("copying into dir {dest_path:?}");
                 let dest_path = dest_path.join(Path::new(src_path.file_name().unwrap()));
-                debug!("target path = {dest_path:?}");
-                let mut dest_handle: <F2 as FloppyDisk>::File =
-                    dest_handle.open(dest, &dest_path).await?;
-
+                trace!("target path = {dest_path:?}");
                 let written = tokio::io::copy(&mut src_handle, &mut dest_handle).await?;
-                debug!("wrote {written} bytes");
+                trace!("wrote {written} bytes");
                 dest_handle.flush().await?;
 
                 // copy permissions
@@ -171,17 +211,29 @@ where
 
             // if dest exists and is a file, copy into it
             if dest_metadata.is_file() {
-                warn!("overwriting dest file {dest_path:?}");
-                let mut dest_handle: <F2 as FloppyDisk>::File =
-                    dest_handle.open(dest, dest_path).await?;
-
+                trace!("overwriting dest file {dest_path:?}");
                 tokio::io::copy(&mut src_handle, &mut dest_handle).await?;
+
+                // copy permissions
+                let src_metadata = src_handle.metadata().await?;
+                let src_permissions = src_metadata.permissions();
+                let mode = <<F1 as FloppyDisk<'_>>::Permissions as FloppyUnixPermissions>::mode(
+                    &src_permissions,
+                );
+
+                let permissions = <<F2 as FloppyDisk>::Permissions>::from_mode(mode);
+                let uid = src_metadata.uid()?;
+                let gid = src_metadata.gid()?;
+
+                <F2 as FloppyDiskUnixExt>::chown(dest, dest_path, uid, gid).await?;
+                <F2 as FloppyDisk>::set_permissions(dest, dest_path, permissions).await?;
+
                 return Ok(());
             }
 
             // if dest exists and is a symlink, log error and return
             if dest_metadata.is_symlink() {
-                warn!("dest path {dest_path:?} is a symlink, skipping copy!");
+                warn!("dest file path {dest_path:?} is a symlink, skipping copy!");
                 return Ok(());
             }
         }
@@ -205,6 +257,9 @@ where
         src_path: &Path,
         dest_path: &Path,
     ) -> Result<()> {
+        let dest_path = dest_path.join(src_path);
+        let dest_path = dest_path.as_path();
+        trace!("creating dir {dest_path:?}");
         dest.create_dir_all(dest_path).await?;
 
         let src_metadata = src.metadata(src_path).await?;
@@ -220,12 +275,14 @@ where
     async fn add_symlink_to_memfs(
         src: &F1,
         dest: &F2,
-        path: &Path,
-        memfs_path: &Path,
+        src_path: &Path,
+        dest_path: &Path,
     ) -> Result<()> {
-        let link = src.read_link(path).await?;
-        debug!("linking {memfs_path:?} to {link:?}");
-        dest.symlink(link, memfs_path.into()).await?;
+        let dest_path = dest_path.join(src_path);
+        let dest_path = dest_path.as_path();
+        let link = src.read_link(src_path).await?;
+        trace!("linking {dest_path:?} to {link:?}");
+        dest.symlink(link, dest_path.into()).await?;
 
         Ok(())
     }
